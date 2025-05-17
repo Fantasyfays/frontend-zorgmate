e27546628d4dfada848cab54c92e49b0eb697167
Fantasyfays <134273551+Fantasyfays@users.noreply.github.com>
2025-05-17 16:45:19 +0200

    yes

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'i15wb9',
   e2e: {
-    baseUrl: "http://localhost:3000", // verander dit naar jouw dev server
+    baseUrl: "http://localhost:3000",
   },
 });
