ecb3f0b6620c397248da0e70a4aad72d8fdf4c6c
Fantasyfays <134273551+Fantasyfays@users.noreply.github.com>
2025-06-03 20:21:54 +0200

    autheraztion navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
                 <Col md={8}>
                     <h1 className="display-4 text-primary fw-bold">Welkom bij ZorgMate!</h1>
                     <p className="lead my-4">
-                        Beheer factures eenvoudig, efficiënt en veilig met ZorgMate.
+                        Beheer faycal eenvoudig, efficiënt en veilig met ZorgMate.
                         Een moderne oplossing voor al je gebruikersbeheer.
                     </p>
                     <Button href="/users" className="btn-custom btn-lg">Bekijk Gebruikers</Button>
